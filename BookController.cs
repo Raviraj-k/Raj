2848009726b67a5be930011cf37fa11ab9640958
Raviraj-k <140150968+Raviraj-k@users.noreply.github.com>
2024-01-16 17:00:06 +0530

    Add files via upload

---
+++ BookController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library_magmt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+    }
+
+
+
+    /*
+     feature :
+
+   1-  user : [ I can be able to login and singup ] 
+          steps : 1 students must able to singup ( add student )
+          steps : 2 Stundets must be able to log in ( request : username and pass .... ) return (uid):
+
+    need : student (dtype)   - CRUD
+           librarian      - CRUD  
+       
+    Deadline : Tomarrow (3 Jan ) 
+
+
+    2 - Book  
+Features : 
+
+    1- Add Book , Delete , Update , Issue book , return book , Request (xyz) 
+    2 -  search by book-name , author , subject (get)
+
+
+    librarian : 
+    show books in library 
+    show borrowed books 
+    show total books 
+     
+    Need : Book CRUD
+     
+     */
+}
